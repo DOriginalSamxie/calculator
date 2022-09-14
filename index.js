62b5530f1d11b1d5d7c616c0ccde6e3969885d8e
DOriginalSamxie <samadrufai@gmail.com>
2022-09-14 23:09:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let output = document.getElementById('output')
+function display(num) {
+  output.value += num
+}
+function calculate() {
+  try {
+    output.value = eval(output.value)
+  } catch (error) {
+    alert('invalid')
+  }
+}
+function Clear() {
+  output.value = ''
+}
+function del(num) {
+  output.value = output.value.slice(0, -1)
+}
